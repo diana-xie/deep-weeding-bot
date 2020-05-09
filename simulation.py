0dcc598e33c9b151545f94866f905496a2e8b8a7
Diana <diana.lynn.xie@gmail.com>
2020-05-09 08:46:40 -0400

    Added Arduino write from Python

--- simulation.py
+++ simulation.py
@@ -1,3 +1,7 @@
+""" """ """ """ """ """ """ """ """ """ """ """
+""" Perform inference & show results """
+""" """ """ """ """ """ """ """ """ """ """ """
+
 import pandas as pd
 from keras.models import load_model
 import numpy as np
@@ -7,19 +11,13 @@ import matplotlib.pyplot as plt
 from time import sleep
 import serial
 
-# Simulator params
-DEBUG_MODE = True
-ARDUINO_PORT = 'COM5'
 
 # Global paths
 OUTPUT_DIRECTORY = "./outputs/"
 LABEL_DIRECTORY = "./labels/"
 MODEL_DIRECTORY = "./models/"
-MODEL_URL = "https://nextcloud.qriscloud.org.au/index.php/s/Y7EhlkVMYCqxdg2/download"
-MODEL_ZIP_FILE = "./models/models.zip"
 IMG_DIRECTORY = "./images/"
-IMG_URL = "https://nextcloud.qriscloud.org.au/index.php/s/a3KxPawpqkiorST/download"
-IMG_ZIP_FILE = "./images/images.zip"
+
 
 # Global variables
 RAW_IMG_SIZE = (256, 256)
@@ -45,18 +43,16 @@ CLASS_NAMES = ['Chinee Apple',
 CLASS_DICT = dict(zip(CLASSES, CLASS_NAMES))
 
 # Load model
-model = load_model('models/resnet.hdf5')
+model = load_model(MODEL_DIRECTORY + 'resnet.hdf5')
 # Load labels
 data = pd.read_csv(LABEL_DIRECTORY + "labels.csv")
 
-# Setup
-filenames = list(data['Filename'])
-actual_labels = list(data['Species'])
-weed_labels = ['Siam Weed', 'Snake weed', 'Prickly acacia', 'Chinee Apple']  # Set weeds
-
-""" """ """ """ """ """ """ """ """ """ """ """
-""" Perform inference & show results """
-""" """ """ """ """ """ """ """ """ """ """ """
+# Simulator params
+DEBUG_MODE = False
+ARDUINO_PORT = 'COM5'
+FILENAMES = list(data['Filename'])
+ACTUAL_LABELS = list(data['Species'])
+WEED_LABELS = ['Siam Weed', 'Snake weed', 'Prickly acacia', 'Chinee Apple']  # Set weeds
 
 
 def show_image(img: np.array, plant_name: str, is_weed: bool = False):
@@ -78,7 +74,7 @@ def make_inference(file_idx: int, model):
     :return: 
     """
     # Load image
-    filename = filenames[file_idx]
+    filename = FILENAMES[file_idx]
     img = imread(IMG_DIRECTORY + filename)
     # Resize to 224x224
     img = resize(img, (224, 224))
@@ -89,17 +85,16 @@ def make_inference(file_idx: int, model):
 
     # Predict label
     prediction = model.predict(img, batch_size=1, verbose=0)
-    y_pred = np.argmax(prediction[0][:7], axis=1)
-    # y_pred[np.max(prediction, axis=1) < 1 / 9] = 8
+    y_pred = np.argmax(prediction[0][:7])
 
     # Show results
     is_weed = False
-    plant_name = actual_labels[file_idx]
+    plant_name = ACTUAL_LABELS[file_idx]
     predicted_name = CLASS_NAMES[y_pred]
-    if predicted_name in weed_labels:
+    if plant_name in WEED_LABELS:
         is_weed = True
 
-    show_image(img=img, plant_name=predicted_name, is_weed=is_weed)
+    show_image(img=img, plant_name=plant_name, is_weed=is_weed)
 
     return predicted_name, plant_name, is_weed
 
@@ -113,25 +108,15 @@ def blink_arduino():
 
 def robot_simulation():
 
-    # store 'N' and 'W' coordinates
-    list_n = []  # store 'N' coordinates
-    list_w = []  # store 'W' coordinates
-
-    # tracker to determine if last coord was same as latest
-    track_lat = 0
-    track_lon = 0
-
-    # counter to present new image
-    counter = 0
-
-    # store labels_predicted
-    labels_predicted = []
-    labels_actual = []
+    labels_predicted, labels_actual = [], []  # store labels_predicted
+    list_n, list_w = [], []  # store 'N', 'W' coordinates
+    track_lat, track_lon = 0, 0  # tracker to determine if last coord was same as latest
+    counter = 0  # counter to present new image
 
     # for debugging inference
     if DEBUG_MODE:
-        while counter < len(filenames):
-            prediction, actual = make_inference(file_idx=counter, model=model)
+        while counter < len(FILENAMES):
+            prediction, actual, is_weed = make_inference(file_idx=counter, model=model)
             labels_predicted.append(prediction)
             labels_actual.append(actual)
             counter += 1
@@ -139,44 +124,50 @@ def robot_simulation():
     # for regular run with Arduino
     else:
 
-        with serial.Serial(ARDUINO_PORT, baudrate=115200, timeout=1) as ser:
+        ser = serial.Serial(ARDUINO_PORT, baudrate=115200, timeout=1)  # as ser:
 
-            # read from the serial output
-            # while counter < len(filenames):
-            while counter < len(filenames[:100]):
+        # read from the serial output
+        while counter < len(FILENAMES):
 
-                line = ser.readline().decode('ascii', errors='replace')
-                response = line.split(',')
+            line = ser.readline().decode('ascii', errors='replace')
+            response = line.split(',')
 
-                if len(response) > 4 and line.find("$GPRMC") == 0:
+            if len(response) > 4 and line.find("$GPRMC") == 0:
 
-                    try:
-                        lat = ((float(response[3]) / 100.00) - (int(float(response[3]) / 100.00))) / 0.6 + int(
-                            float(response[3]) / 100.00)  # response[4]  # 'N'
-                        lon = ((float(response[5]) / 100.00) - (int(float(response[5]) / 100.00))) / 0.6 + int(
-                            float(response[5]) / 100.00)  # response[6]  # 'W'
-                        message = "{} {}, {} {}".format(str(lat), response[4], str(lon), response[6])
-                        print(message)
-                        list_n.append(lat)
-                        list_w.append(lon)
+                try:
+                    lat = ((float(response[3]) / 100.00) - (int(float(response[3]) / 100.00))) / 0.6 + int(
+                        float(response[3]) / 100.00)  # response[4]  # 'N'
+                    lon = ((float(response[5]) / 100.00) - (int(float(response[5]) / 100.00))) / 0.6 + int(
+                        float(response[5]) / 100.00)  # response[6]  # 'W'
+                    message = "{} {}, {} {}".format(str(lat), response[4], str(lon), response[6])
+                    print(message)
+                    list_n.append(lat)
+                    list_w.append(lon)
 
-                        # robot moved, snapshots new "scene", now classify weeds
-                        if (lat != track_lat) or (lon != track_lon):
-                            print("'Saw' image, so running plant detection and classification")
-                            # prediction, actual = make_inference(file_idx=counter, model=model)
-                            prediction, actual, is_weed = make_inference(file_idx=counter, model=model)
-                            labels_predicted.append(prediction)
-                            labels_actual.append(actual)
+                    # robot moved, snapshots new "scene", now classify weeds
+                    if (lat != track_lat) or (lon != track_lon):
+                        print("'Saw' image, so running plant detection and classification")
+                        # prediction, actual = make_inference(file_idx=counter, model=model)
+                        prediction, actual, is_weed = make_inference(file_idx=counter, model=model)
+                        labels_predicted.append(prediction)
+                        labels_actual.append(actual)
 
-                            counter += 1
+                        try:
+                            if is_weed:
+                                blink_arduino()
+                        except Exception as ex:
+                            print("Error in making Arduino blink: {}".format(ex))
+                            raise ex
 
-                        # track, to see if robot "moves" next round
-                        track_lat, track_lon = lat, lon
+                        counter += 1
 
-                    except Exception as ex:
-                        print('Could not get lat & lon, due to {}'.format(ex))
+                    # track, to see if robot "moves" next round
+                    track_lat, track_lon = lat, lon
 
-                sleep(.05)
+                except Exception as ex:
+                    print('Could not get lat & lon, due to {}'.format(ex))
+
+            sleep(.05)
 
     # pd.DataFrame([labels_predicted, labels_actual], columns=['predicted', 'actual'])
     print("Finished simulation")
